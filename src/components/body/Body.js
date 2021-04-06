6fee55e76102fe283d6d69658939e0f89fb85394
Philip Daniel Oghenetega <tegadaniels8@gmail.com>
2021-04-06 21:52:24 +0100

    committed

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,37 +3,48 @@ import {Grid} from '@material-ui/core'
 import axios from 'axios'
 import GoodsGrid from '../../grid/goodsGrid/GoodsGrid'
 import Header from '../header/Header'
+import Cart from '../cart/Cart'
 
 function Body() {
     const [items, setItems] = useState([])
     const [cart, setCart] = useState([])
-    const [isLoading, setisLoading] = useState(true)
 
-    useEffect(() =>{
-        const fetchItems = async () =>{
-            const result = await axios(`https://fakestoreapi.com/products`)
-            console.log(result.data)
 
-            setItems(result.data)
-            setisLoading(false)
-        }
-        fetchItems()
-    }, [])
-    const addToCart = (items) =>{
+
+    const fetchItems = async () =>{
+        const result = await axios(`https://fakestoreapi.com/products`)
+        console.log(result.data)
+
+        setItems(result.data)
+
+    }
+
+    const addToCart =  (items) =>{
         console.log(cart.length)
         console.log('Before add cart!!!!!')
-        setCart(cart => [...cart, items])
+       const _cart = [...cart];
+       _cart[_cart.length] = items;
+       console.log(_cart)
+
+
+        setCart(_cart)
         console.log('After adding cart')
-        console.log(addToCart.length)
+        console.log(_cart.length)
+        console.log(_cart)
     }
+
+    useEffect(() =>{
+        fetchItems()
+    }, [])
+
     return (
 
     <Grid container direction = "column">
-        <Header  addCart={cart.length}/>
+        <Header addCart={cart.length}/>
         <Grid item container >
             <Grid item xs={false} sm={2}/>
         <Grid item xs={12} sm ={8}>
-       <GoodsGrid isLoading={isLoading} items={items} addCart={addToCart}/>
+       <GoodsGrid  items={items} addCart={addToCart}/>
         </Grid>
         <Grid item xs={false} sm={2}/>
         </Grid>
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {Container, Typography, Button, Grid} from '@material-ui/core';
+const Cart = () => {
+    // const classes = useStyles();
+    const isEmpty = true;
 
-export default function Cart() {
+    const EmptyCart = ({cart}) =>(
+        <Typography variant = "subtitle1">
+            Cart Currently empty, add items
+        </Typography>
+    )
+
+    const FilledCart = () =>(
+        <>
+        <Grid conatiner spacing = {3}>
+
+        </Grid>
+        </>
+    )
     return (
-        <div>
-            <h2> Working</h2>
-        </div>
+        <Container>
+            <div >
+            <Typography  variant="h3">
+                Your Shopping Cart
+            </Typography>
+            {isEmpty ? <EmptyCart/>: <FilledCart/>}
+            </div>
+        </Container>
     )
 }
+
+export default Cart
